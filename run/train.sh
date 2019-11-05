35a8bed8be7ab7316d0f2fff02f48051bee01cfd
yeyupiaoling <yeyupiaoling@foxmail.com>
2019-11-05 19:16:14 +0800

    debuging

--- run/train.sh
+++ run/train.sh
@@ -17,7 +17,7 @@ python -u train.py \
 --learning_rate=5e-4 \
 --max_duration=27.0 \
 --min_duration=0.0 \
---test_off=True \
+--test_off=False \
 --use_sortagrad=True \
 --use_gru=True \
 --use_gpu=True \
