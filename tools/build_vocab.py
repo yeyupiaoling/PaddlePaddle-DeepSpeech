b994ce1dee6d905728a341df2680568d8300c678
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-08-08 13:47:12 +0800

    fix build vocab bug

--- tools/build_vocab.py
+++ tools/build_vocab.py
@@ -19,7 +19,7 @@ add_arg = functools.partial(add_arguments, argparser=parser)
 # yapf: disable
 add_arg('count_threshold',  int,    0,  "Truncation threshold for char counts.")
 add_arg('vocab_path',       str,    './dataset/zh_vocab.txt', "Filepath to write the vocabulary.")
-add_arg('manifest_path',   str,    './dataset/manifest.train', "Filepaths of manifests for building vocabulary. You can provide multiple manifest files.")
+add_arg('manifest_paths',   str,    './dataset/manifest.train,./dataset/manifest.dev,', "Filepaths of manifests for building vocabulary. You can provide multiple manifest files.")
 # yapf: disable
 args = parser.parse_args()
 
@@ -35,7 +35,9 @@ def main():
     print_arguments(args)
 
     counter = Counter()
-    count_manifest(counter, args.manifest_path)
+    manifest_paths = [path for path in args.manifest_paths.split(',')]
+    for manifest_path in manifest_paths:
+        count_manifest(counter, manifest_path)
 
     count_sorted = sorted(counter.items(), key=lambda x: x[1], reverse=True)
     with codecs.open(args.vocab_path, 'w', 'utf-8') as fout:
