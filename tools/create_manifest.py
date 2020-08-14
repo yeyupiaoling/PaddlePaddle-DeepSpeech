5662ff593852bc2eb8ca64fc88fb3bc1121fee84
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-08-14 09:03:20 +0800

    fix not create test manifest bug

--- tools/create_manifest.py
+++ tools/create_manifest.py
@@ -56,10 +56,8 @@ def create_manifest(annotation_path, manifest_path_prefix):
     f_test = codecs.open(os.path.join(manifest_path_prefix, 'manifest.test'), 'w', 'utf-8')
     for i, line in enumerate(json_lines):
         if i % 500 == 0:
-            if i % 2 == 0:
-                f_dev.write(line + '\n')
-            else:
-                f_test.write(line + '\n')
+            f_dev.write(line + '\n')
+            f_test.write(line + '\n')
         else:
             f_train.write(line + '\n')
     f_train.close()
